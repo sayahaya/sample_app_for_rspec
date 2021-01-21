cf44406c49a5425d01086dd00c2e9adbe708a23d
sayahaya <sayahaya1129@gmail.com>
2021-01-22 08:37:00 +0900

    Add: 自力で分かる範囲のテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "Titlet #{n}" }
+    status { 0 }  
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+  
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      expect(FactoryBot.build(:task)).to be_valid
+    end
+    
+    it 'is invalid without title' do
+      task = Task.new(title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank") 
+    end
+    
+    it 'is invalid without status' do
+      task = Task.new(status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+    
+    it 'is invalid with a duplicate title' do
+      user = User.create(:user)
+      user.tasks.create(:task)
+      new_task = user.tasks.build(:task)
+      new_task.valid?
+      expect(new_task.errors[:title]).to include("has already been taken")
+    end
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+    it 'is valid with another title' do end
+  end
